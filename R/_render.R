3226de3168caa943449f403e88698efb16a353da
Leon Reteig <leonreteig@gmail.com>
2019-10-25 15:35:10 +0200

    Manage internal scripts

--- R/_render.R
+++ R/_render.R
@@ -1,6 +1,7 @@
+# N.B. This is for internal use and not distributed with the amsterdown package
+
 setwd("inst/rmarkdown/templates/thesis/skeleton") # change working directory to where the book lives
 file.rename("skeleton.Rmd", "index.Rmd") # main file needs to be named index.html for the website to show correctly
 # make the book; move to "docs" folder in root
 bookdown::render_book("index.Rmd", output_format = 'bookdown::pdf_book', output_dir = "../../../../../docs") # pdf first, so it's linked in the html
 bookdown::render_book("index.Rmd", output_format = 'bookdown::gitbook', output_dir = "../../../../../docs")
-
---
+++ R/_usethis.R
@@ -0,0 +1,39 @@
+# N.B. This is for internal use and not distributed with the amsterdown package
+
+available::available("amsterdown") # check whether package names is avaiable, and there's no conflicting interpretations
+
+# initial setup
+usethis::create_package("~/amsterdown") # make package skeleton
+usethis::use_git() # add version control
+usethis::use_github() # add github repository
+
+# add rmarkdown template skeleton
+usethis::use_rmarkdown_template(template_name = "UvA thesis",
+                       template_dir = "thesis",
+                       template_description = "Template for creating a University of Amsterdam PhD thesis using RMarkdown and the bookdown package.")
+
+# add license info
+usetthis::use_cc0_license() # public domain, as there's not much code in this package
+
+# add dependency
+usethis::use_package("bookdown")
+
+# add README
+usethis::use_readme_rmd()
+
+# add continuous integration to:
+  # - check package automatically
+  # - deploy sample output automatically
+usethis::use_travis(ext = "com")
+
+# hide script that deploys from package
+usethis::use_build_ignore(c("R/_render.r", "R/_usethis.R"))
+
+# add package-level documentation
+usethis::use_package_doc()
+
+# signal development status of package
+usethis::use_lifecycle()
+usethis::use_lifecycle_badge("experimental")
+
+
