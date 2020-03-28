b2a9dbf601570e31838cc3c896d03629fdba2dc5
Leon Reteig <leonreteig@gmail.com>
2020-03-28 11:52:54 +0100

    Build documentation on travis

--- R/_render.R
+++ R/_render.R
@@ -3,9 +3,12 @@
 rmarkdown::draft('index.Rmd', template = 'thesis', package = 'amsterdown', create_dir = TRUE, edit = FALSE) # create new draft based off template
 setwd("index")
 
-# pdfs first, so they get linked in the html; move to "docs" folder in root
-bookdown::render_book("index.Rmd", output_format = 'bookdown::pdf_book', output_dir = "../docs")
-file.rename(c("../docs/thesis.tex", "../docs/thesis.pdf"), c("../docs/thesis_A4.tex", "../docs/thesis_A4.pdf"))
+target_dir <- "../docs/dev/sample_output"
+
+# pdfs first, so they get linked in the html; move to ouput folder in root
+bookdown::render_book("index.Rmd", output_format = 'bookdown::pdf_book', output_dir = target_dir, clean_envir = FALSE)
+file.rename(c(file.path(target_dir, "thesis.tex"), file.path(target_dir, "thesis.pdf")),
+            c(file.path(target_dir, "thesis_A4.tex"), file.path(target_dir, "thesis_A4.pdf")))
 
 # change yaml in the index.Rmd file, to render to book size
 f <- readLines("index.Rmd")
@@ -14,16 +17,17 @@ f[grepl("^book_size:",f)] <- "book_size: true" # change the line containing the 
 writeLines(f, "index.Rmd")
 
 # render 2nd pdf
-bookdown::render_book("index.Rmd", output_format = 'bookdown::pdf_book', output_dir = "../docs")
-file.rename(c("../docs/thesis.tex", "../docs/thesis.pdf"), c("../docs/thesis_B5.tex", "../docs/thesis_B5.pdf"))
+bookdown::render_book("index.Rmd", output_format = 'bookdown::pdf_book', output_dir = file.path(target_dir), clean_envir = FALSE)
+file.rename(c(file.path(target_dir, "thesis.tex"), file.path(target_dir, "thesis.pdf")),
+            c(file.path(target_dir, "thesis_B5.tex"), file.path(target_dir, "thesis_B5.pdf")))
 
 # render the html version
-bookdown::render_book("index.Rmd", output_format = 'bookdown::gitbook', output_dir = "../docs")
+bookdown::render_book("index.Rmd", output_format = 'bookdown::gitbook', output_dir = target_dir, clean_envir = FALSE)
 
 # patch html files to point to two pdfs
 old_line <- '"download": null'
 new_line <- '"download": [["thesis_A4.pdf", "PDF: A4"], ["thesis_B5.pdf", "PDF: book"]]'
-for (html_file in list.files("../docs/", ".html", full.names = TRUE)) {
+for (html_file in list.files(target_dir, ".html", full.names = TRUE)) {
   f <- readLines(html_file)
   f <- gsub(old_line, new_line,f)
   writeLines(f, html_file)
